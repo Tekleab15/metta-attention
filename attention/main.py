9b1b5cefec7ce3fa09943df434e3a862afc8daac
Tekleab15 <151877564+Tekleab15@users.noreply.github.com>
2025-03-20 15:04:08 +0000

    adjust base directory

--- attention/main.py
+++ attention/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-base_dir = sys.path.append(os.path.dirname(os.path.abspath(__file__),".."))
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(base_dir, "attention"))
 sys.path.append(os.path.join(base_dir, "attention-bank"))
 
